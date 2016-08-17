3a4a3df98e4355917343e6a23fd1b08de7128dcf
Joseph Paulo V. Los Baños <paulo.losbanos@tappcommerce.com>
2016-08-17 11:53:27 +0800

    basic body of auth feature

---
+++ app/src/main/java/atlas/com/trainerapp/authentication/models/User.java
@@ -0,0 +1,7 @@
+package atlas.com.trainerapp.authentication.models;
+
+/**
+ * Created by paulo.losbanos on 17/08/2016.
+ */
+public class User {
+}
---
+++ app/src/main/java/atlas/com/trainerapp/authentication/presenters/AuthPresenter.java
@@ -0,0 +1,7 @@
+package atlas.com.trainerapp.authentication.presenters;
+
+/**
+ * Created by paulo.losbanos on 17/08/2016.
+ */
+public class AuthPresenter {
+}
