5fb8c774b0bdf5d7228a7acbfed76f3259aba1c1
Joseph Paulo V. Los Baños <paulo.losbanos@tappcommerce.com>
2016-08-29 21:17:30 +0800

    trainer card template + initial card for main team

--- app/src/main/java/atlas/com/trainerapp/widgets/TATextView.java
+++ app/src/main/java/atlas/com/trainerapp/widgets/TATextView.java
@@ -1,12 +1,14 @@
 package atlas.com.trainerapp.widgets;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Typeface;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
 import com.jakewharton.rxbinding.view.RxView;
 
+import atlas.com.trainerapp.R;
 import rx.Observable;
 
 /**
@@ -18,24 +20,46 @@ public class TATextView extends TextView {
 
     public TATextView(Context context) {
         super(context);
-        init(context);
+        init(context,null);
     }
 
     public TATextView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init(context);
+        init(context,attrs);
     }
 
     public TATextView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init(context);
+        init(context,attrs);
     }
 
-    private void init(Context context) {
+    private void init(Context context, AttributeSet attrs) {
         mContext = context;
         mTypeFace = Typeface.createFromAsset(mContext.getAssets(),"fonts/Montserrat-Regular.otf");
-
+        String font = "";
+        if(attrs != null) {
+            TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.TATextView, 0, 0);
+            try {
+                font = ta.getString(R.styleable.TATextView_font);
+            } finally {
+                ta.recycle();
+            }
+            chooseFont(font);
+        }
         setTypeface(mTypeFace);
+    }
+
+    private void chooseFont(String font) {
+        switch (font) {
+            case BARIOL:
+                mTypeFace = Typeface.createFromAsset(mContext.getAssets(),"fonts/Bariol.ttf");
+                break;
+            case MONTSERRAT:
+                mTypeFace = Typeface.createFromAsset(mContext.getAssets(),"fonts/Montserrat-Regular.otf");
+                break;
+            default:
+                mTypeFace = Typeface.createFromAsset(mContext.getAssets(),"fonts/Montserrat-Regular.otf");
+        }
     }
 
     public Typeface typeFace() {
@@ -45,4 +69,8 @@ public class TATextView extends TextView {
     public Observable<Void> clickObservable() {
         return RxView.clicks(this);
     }
+
+    public static final String BARIOL = "bariol";
+    public static final String MONTSERRAT = "montserrat";
+
 }
